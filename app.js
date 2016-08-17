05e70b70e2268563e4c3d013688303d3ec7153f3
Chandan Singh <chandan2495@gmail.com>
2016-08-18 00:51:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require('express');
-
+var bodyParser = require('body-parser')
 var path = require('path');
 
 app = express();
 
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, 'dist')));
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/dist'));
 
 var port = process.env.PORT || 5000;
 
 app.listen(port);
 
-console.log('server started '+port);
+console.log('server started ' + port);
